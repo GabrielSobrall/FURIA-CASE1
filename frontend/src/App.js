e28b1d7678db16611daa0ac5fb3822c99e24e91a
gabrielsobrall <gabrielsobral2018@outlook.com>
2025-05-01 23:04:27 -0300

    LoginAtt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import React, {useState} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-function App(){
-    
-  }
+function App() {
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/Home" element={<Home />} />
+    </Routes>
+  </Router>
+  );
+}
+
 
 export default App
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react';
-function Login(){
-    const [texto, setTexto] = useState('');
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-    function handleChange(event) {
-      setTexto(event.target.value);
-    }
-  
-    return (
-      <div>
-        <input type="text" value={texto} onChange={handleChange} placeholder="Digite seu nick" />
-        <button>Entrar</button>
-      </div>
-    );
+function Login() {
+  const [usuario, setUsuario] = useState("");
+  const navigate = useNavigate();
+
+  function atualizar(event) {
+    setUsuario(event.target.value);
   }
 
-  export default Login;
+  function mandarLogin() {
+    navigate("/Home", { state: { nick: usuario } });
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={usuario}
+        onChange={atualizar}
+        placeholder="Digite seu nick"
+      />
+      <button onClick={mandarLogin}>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
