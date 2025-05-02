1f2a189bee7ee1dc1fe98ad11170c66695787a7b
gabrielsobrall <gabrielsobral2018@outlook.com>
2025-05-01 23:39:02 -0300

    Passandoinformaçõeshome

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import {useLocation} from 'react-router-dom';
+
 function Home(){
+    const location = useLocation();
+    const nick =location.state?.nick;
+    
+    
 
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,43 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import styled from "styled-components"; 
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: #f2f2f2;
+`;
+
+const Titulo = styled.h1`
+  margin-bottom: 20px;
+  color: #333;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 250px;
+  font-size: 16px;
+`;
+
+const Botao = styled.button`
+  padding: 10px 20px;
+  background-color: #0077cc;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+
+  &:hover {
+    background-color: #005fa3;
+  }
+`;
 
 function Login() {
   const [usuario, setUsuario] = useState("");
@@ -15,15 +52,16 @@ function Login() {
   }
 
   return (
-    <div>
-      <input
+    <Container>
+      <Titulo>Faça o Login</Titulo>
+      <Input
         type="text"
         value={usuario}
         onChange={atualizar}
         placeholder="Digite seu nick"
       />
-      <button onClick={mandarLogin}>Entrar</button>
-    </div>
+      <Botao onClick={mandarLogin}>Entrar</Botao>
+    </Container>
   );
 }
 
